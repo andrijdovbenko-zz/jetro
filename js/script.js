fc0e01d29f1c69c671aa19189143d00853089f70
Andrii Dovbenko <andrijdovbenko@gmail.com>
2017-02-20 07:52:37 +0200

    with about us, portfolio V4

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
 
-$(document).ready(function(){
-    /*nav on small divices*/
+/*nav on small divices*/
 
-    $("#navigation").click(function(){
-        $("header nav").slideToggle("fast");
-    });
+$("#navigation").click(function(){
+    $("header nav").slideToggle("fast");
+});
 
-    /*filter*/
+/*filter*/
 
 
-    $('main .filter span[data-filter]').click(function(event){
-        let attr = $(event.target).attr("data-filter");
+$('main .filter span[data-filter]').click(function(event){
+    let attr = $(event.target).attr("data-filter");
 
-            $('main .filter span').removeClass('active');
-            $('main .filter span[data-filter="' + attr + '"]').addClass("active");
-            $('.works .item').parent().hide();
+        $('main .filter span').removeClass('active');
+        $('main .filter span[data-filter="' + attr + '"]').addClass("active");
+        $('.works .item').parent().hide();
 
-        if (attr == 'all'){
-            $('.works div[data-filter]').parent().fadeIn(); 
-        }
-        else{
-            $('.works div[data-filter="' + attr + '"]').parent().fadeIn();  
-        }
+    if (attr == 'all'){
+        $('.works div[data-filter]').parent().fadeIn(); 
+    }
+    else{
+        $('.works div[data-filter="' + attr + '"]').parent().fadeIn();  
+    }
 
-     });
+ });
 
     
-});
 	
