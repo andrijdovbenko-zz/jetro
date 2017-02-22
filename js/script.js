4d22035b3c58ecd07eee21e1f869979bc362331d
Andrii Dovbenko <andrijdovbenko@gmail.com>
2017-02-22 09:01:25 +0200

    with fixed porfolio

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ $('main .filter span[data-filter]').click(function(event){
 
         $('main .filter span').removeClass('active');
         $('main .filter span[data-filter="' + attr + '"]').addClass("active");
-        $('.works .item').parent().hide();
+        $('.works .item').parent().parent().hide();
 
     if (attr == 'all'){
-        $('.works div[data-filter]').parent().fadeIn(); 
+        $('.works div[data-filter]').parent().parent().fadeIn(); 
     }
     else{
-        $('.works div[data-filter="' + attr + '"]').parent().fadeIn();  
+        $('.works div[data-filter="' + attr + '"]').parent().parent().fadeIn();  
     }
 
  });
